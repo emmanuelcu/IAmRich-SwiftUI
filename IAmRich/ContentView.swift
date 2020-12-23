ecc0a5636e8609230d153b846c99ed8c41a6f0ef
Emmanuel Zoolander <12421140+emmanuelcu@users.noreply.github.com>
2020-12-23 11:59:16 -0600

    The layout of the app has been finished, by just using SwiftUI.

--- IAmRich/ContentView.swift
+++ IAmRich/ContentView.swift
@@ -9,14 +9,27 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello this is swift UI!")
-            .padding()
+        ZStack{
+            Color(.systemTeal)
+                .edgesIgnoringSafeArea(.all)
+            VStack {
+                Text("I Am Rich")
+                    .font(.system(size: 40))
+                    .fontWeight(.bold)
+                    .foregroundColor(Color.white)
+                    .padding()
+                Image("diamond")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .frame(width: 200, height: 200, alignment: .center)
+            }
+        }
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        ContentView().previewDevice(PreviewDevice(rawValue: "iPhone 12"))
         
     }
 }
