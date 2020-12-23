1997d9ed25c883162ff8d31d14c269cfa630a482
Emmanuel Zoolander <12421140+emmanuelcu@users.noreply.github.com>
2020-12-23 10:29:59 -0600

    Initial Commit

---
+++ IAmRich/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  IAmRich
+//
+//  Created by Emmanuel Cuevas on 23/12/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
